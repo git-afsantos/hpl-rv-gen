07226a2aeb6be30441b0ce68805bb3ec00998efd
André Santos <contact.andre.santos@gmail.com>
2021-02-11 11:21:33 +0000

    [WIP] fixing tests

--- tests/test_monitor_classes.py
+++ tests/test_monitor_classes.py
@@ -30,7 +30,7 @@ E_BEHAVIOUR = 3
 E_TRIGGER = 4
 E_SPAM = 5
 
-# MsgRecord = namedtuple("MsgRecord", ('topic', 'timestamp', 'msg'))
+MsgRecord = namedtuple("MsgRecord", ('topic', 'timestamp', 'msg'))
 
 ActivatorEvent = namedtuple('ActivatorEvent',
     ('event', 'topic', 'msg', 'state'))
@@ -80,7 +80,7 @@ def new_spam(topic, msg):
 TimerEvent = namedtuple('TimerEvent',
     ('event', 'state', 'drops'))
 
-def new_timer(state, drops=0):
+def new_timer(state=None, drops=0):
     # state: int (target state)
     # drops: int (triggers dropped)
     return TimerEvent(E_TIMER, state, drops)
@@ -162,16 +162,16 @@ def absence_properties():
     traces.append([ new_spam('b', p2d()) ])
     traces.append([
         new_spam('b', p2d(x=-1)),
-        new_timer(STATE_ACTIVE),
+        new_timer(),
         new_spam('b', p2d(x=-2)),
     ])
     # invalid
     traces.append([ new_behaviour(p2d(x=1), STATE_FALSE) ])
     traces.append([
         new_spam('b', p2d()),
-        new_timer(STATE_ACTIVE),
+        new_timer(),
         new_behaviour(p2d(x=1), STATE_FALSE),
-        new_timer(STATE_FALSE),
+        new_timer(),
         new_spam('b', p2d()),
     ])
     yield (text, traces)
@@ -206,6 +206,58 @@ def all_types_of_property():
 # Test Loop
 ###############################################################################
 
+def state_name(s):
+    if s == STATE_FALSE:
+        return 'FALSE'
+    if s == STATE_TRUE:
+        return 'TRUE'
+    if s == STATE_OFF:
+        return 'OFF'
+    if s == STATE_INACTIVE:
+        return 'INACTIVE'
+    if s == STATE_ACTIVE:
+        return 'ACTIVE'
+    if s == STATE_SAFE:
+        return 'SAFE'
+    return 'STATE {}'.format(s)
+
+def pretty_trace(trace):
+    s = []
+    t = 0
+    for e in trace:
+        t += 1
+        etype = e[0]
+        if etype == E_TIMER:
+            s.append('@ {}: (Timer, -{}) -> {}'.format(
+                t, e.drops, state_name(e.state)))
+        elif etype == E_ACTIVATOR:
+            s.append("@ {}: (Activator) '{}' {} -> {}".format(
+                t, e.topic, e.msg, state_name(e.state)))
+        elif etype == E_TERMINATOR:
+            s.append("@ {}: (Terminator) '{}' {} -> {}".format(
+                t, e.topic, e.msg, state_name(e.state)))
+        elif etype == E_BEHAVIOUR:
+            s.append("@ {}: (Behaviour) '{}' {} -> {}".format(
+                t, e.topic, e.msg, state_name(e.state)))
+        elif etype == E_TRIGGER:
+            s.append("@ {}: (Trigger) '{}' {} -> {}".format(
+                t, e.topic, e.msg, state_name(e.state)))
+        else:
+            s.append("@ {}: (Spam) '{}' {}".format(
+                t, e.topic, e.msg))
+    return "\n".join(s)
+
+def pretty_monitor(m):
+    return "\n".join((
+        'm._state = {}'.format(m._state),
+        'm.time_launch = {}'.format(m.time_launch),
+        'm.time_shutdown = {}'.format(m.time_shutdown),
+        'm.time_state = {}'.format(m.time_state),
+        'm.witness = {}'.format(m.witness),
+        'm._pool = {}'.format(getattr(m, '_pool', None)),
+    ))
+
+
 class TestMonitorClasses(unittest.TestCase):
     #def __init__(self):
     def setUp(self):
@@ -221,7 +273,8 @@ class TestMonitorClasses(unittest.TestCase):
             m = self._make_monitor(py)
             for trace in traces:
                 n += 1
-                self._update_debug_string(text, trace, n)
+                self.hpl_string = text
+                self._set_trace_string(trace, n)
                 self._launch(hp, m)
                 time = 0
                 for event in trace:
@@ -229,10 +282,12 @@ class TestMonitorClasses(unittest.TestCase):
                     self._dispatch(m, event, time)
                 self._shutdown(m)
                 self._reset()
-        print('Tested {} examples.'.format(self._examples))
+        print('Tested {} examples.'.format(n))
 
     def _reset(self):
         self.debug_string = ''
+        self.trace_string = ''
+        self.hpl_string = ''
         self.entered_scope = []
         self.exited_scope = []
         self.found_success = []
@@ -245,6 +300,7 @@ class TestMonitorClasses(unittest.TestCase):
         m.on_exit_scope = self._on_exit
         m.on_success = self._on_success
         m.on_violation = self._on_failure
+        self._update_debug_string(m, -1)
         assert m._state == STATE_OFF, self.debug_string
         assert m.verdict is None, self.debug_string
         assert not m.witness, self.debug_string
@@ -255,6 +311,7 @@ class TestMonitorClasses(unittest.TestCase):
 
     def _launch(self, hp, m):
         m.on_launch(0)
+        self._update_debug_string(m, 0)
         if hp.scope.is_global or hp.scope.is_until:
             assert len(self.entered_scope) == 1, self.debug_string
             assert self.entered_scope[0] == 0, self.debug_string
@@ -271,6 +328,7 @@ class TestMonitorClasses(unittest.TestCase):
         assert m.time_shutdown < 0, self.debug_string
         try:
             m.on_launch(0)
+            self._update_debug_string(m, 0)
             assert False, self.debug_string
         except RuntimeError:
             pass # expected
@@ -294,6 +352,7 @@ class TestMonitorClasses(unittest.TestCase):
         n = len(self.entered_scope)
         assert m._state == STATE_INACTIVE, self.debug_string
         consumed = self._dispatch_msg(m, event.topic, event.msg, time)
+        self._update_debug_string(m, time)
         assert consumed, self.debug_string
         assert len(self.entered_scope) == n + 1, self.debug_string
         assert self.entered_scope[-1] == time, self.debug_string
@@ -312,6 +371,7 @@ class TestMonitorClasses(unittest.TestCase):
         n = len(self.exited_scope)
         assert m._state > STATE_INACTIVE, self.debug_string
         consumed = self._dispatch_msg(m, event.topic, event.msg, time)
+        self._update_debug_string(m, time)
         assert consumed, self.debug_string
         assert len(self.exited_scope) == n + 1, self.debug_string
         assert self.exited_scope[-1] == time, self.debug_string
@@ -326,6 +386,7 @@ class TestMonitorClasses(unittest.TestCase):
         b = len(self.exited_scope)
         assert m._state == STATE_ACTIVE, self.debug_string
         consumed = self._dispatch_msg(m, event.topic, event.msg, time)
+        self._update_debug_string(m, time)
         assert consumed, self.debug_string
         assert len(self.entered_scope) == a, self.debug_string
         assert len(self.exited_scope) == b, self.debug_string
@@ -336,6 +397,7 @@ class TestMonitorClasses(unittest.TestCase):
         b = len(self.exited_scope)
         k = -1 if not hasattr(m, '_pool') else len(m._pool)
         consumed = self._dispatch_msg(m, event.topic, event.msg, time)
+        self._update_debug_string(m, time)
         assert consumed, self.debug_string
         assert len(self.entered_scope) == a, self.debug_string
         assert len(self.exited_scope) == b, self.debug_string
@@ -356,6 +418,7 @@ class TestMonitorClasses(unittest.TestCase):
         s = m._state
         t = m.time_state
         consumed = self._dispatch_msg(m, event.topic, event.msg, time)
+        self._update_debug_string(m, time)
         assert not consumed, self.debug_string
         assert len(self.entered_scope) == a, self.debug_string
         assert len(self.exited_scope) == b, self.debug_string
@@ -374,37 +437,45 @@ class TestMonitorClasses(unittest.TestCase):
         a = len(self.entered_scope)
         b = len(self.exited_scope)
         k = len(getattr(m, '_pool', ()))
+        s = m._state
         m.on_timer(time)
+        self._update_debug_string(m, time)
         assert len(self.entered_scope) == a, self.debug_string
         assert len(self.exited_scope) == b, self.debug_string
         assert len(getattr(m, '_pool', ())) <= k, self.debug_string
-        assert m._state == event.state, self.debug_string
         if event.state == STATE_TRUE:
             assert len(self.found_success) == 1, self.debug_string
             assert self.found_success[0][0] == time, self.debug_string
             assert self.found_success[0][1] == m.witness, self.debug_string
             assert m.verdict is True, self.debug_string
             assert m.time_state == time, self.debug_string
+            assert m._state == event.state, self.debug_string
         elif event.state == STATE_FALSE:
             assert len(self.found_failure) == 1, self.debug_string
             assert self.found_failure[0][0] == time, self.debug_string
             assert self.found_failure[0][1] == m.witness, self.debug_string
             assert m.verdict is False, self.debug_string
             assert m.time_state == time, self.debug_string
-        else:
+            assert m._state == event.state, self.debug_string
+        elif event.state is not None:
             assert not self.found_success, self.debug_string
             assert not self.found_failure, self.debug_string
             assert m.verdict is None, self.debug_string
             assert not m.witness, self.debug_string
+            assert m._state == event.state, self.debug_string
+        else:
+            assert m._state == s, self.debug_string
 
     def _shutdown(self, m):
         m.on_shutdown(1000)
+        self._update_debug_string(m, 1000)
         assert m._state == STATE_OFF, self.debug_string
         assert m.time_launch == 0, self.debug_string
         assert m.time_state >= 0, self.debug_string
         assert m.time_shutdown == 1000, self.debug_string
         try:
             m.on_shutdown(2000)
+            self._update_debug_string(m, 2000)
             assert False, self.debug_string
         except RuntimeError:
             pass # expected
@@ -448,11 +519,17 @@ class TestMonitorClasses(unittest.TestCase):
     def _on_failure(self, stamp, witness):
         self.found_failure.append((stamp, witness))
 
-    def _update_debug_string(self, text, trace, n):
+    def _set_trace_string(self, trace, n):
+        self.trace_string = '[Example #{}]:\n{}'.format(n, pretty_trace(trace))
+
+    def _update_debug_string(self, m, time):
         self.debug_string = ('failed for the following test'
             '\n  [HPL]: {}'
-            '\n  [Example #{}]: {}'
-        ).format(text, n, trace)
+            '\n  {}'
+            '\n  [Timestamp]: {}'
+            '\n  [Monitor]:'
+            '\n{}'
+        ).format(self.hpl_string, self.trace_string, time, pretty_monitor(m))
 
 
 if __name__ == '__main__':
